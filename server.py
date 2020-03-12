8a243f97c5e35fc782d3648677fc5826103c3a04
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2020-03-12 15:02:35 +0100

    Greater: Finished Image UI

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import cgi
 import os
+import shutil
 import subprocess
 
 
@@ -28,7 +29,33 @@ class MyHandler(BaseHTTPRequestHandler):
                     title = 'No title'
                 returnstring += self.query_create_result(url, title)
         except Exception as e: # We get here if we have not crawled yet
-            print(str(e))
+            returnstring += '<div style="font-size: normal; line-height: 1.57; color: #222">We have not crawled the internet yet. Please try again soon!</div>'
+
+        returnstring += '</div></body></html>'
+        return returnstring
+
+    def imagequery_create_result(self, path):
+        return '<div style="line-height: 1.2; margin-bottom: 40px; color: #222"> \
+                    <a href="{0}" style="font-size: normal; line-height: 1.57; color: #222; cursor: pointer;"> \
+                        <div> \
+                            <img src={0}> \
+                        </div> \
+                    <cite style="font-style: normal; text-decoration: none;">{0}</cite> \
+                    </a> \
+                </div>'.format(path)
+
+    def imagequery_process(self, file):
+        returnstring = '<html><body><center><p><br><p><br><font face=arial size=7 color=darkblue>LIACS Search Image Results</font></center><div style="margin-left: 180px; font-family: arial,sans-serif;">'
+        os.makedirs('uploaded', exist_ok=True)
+        querypath = os.path.join('uploaded', file.filename)
+        with open(querypath, 'wb') as fout:
+            shutil.copyfileobj(file.file, fout, 100000)
+
+        try:
+            output = bytes.decode(subprocess.check_output(['./piccompare', querypath]))
+            for path in output.splitlines():
+                returnstring += self.imagequery_create_result(path)
+        except Exception as e: # We get here if we have not crawled yet
             returnstring += '<div style="font-size: normal; line-height: 1.57; color: #222">We have not crawled the internet yet. Please try again soon!</div>'
 
         returnstring += '</div></body></html>'
@@ -36,10 +63,13 @@ class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         file_to_fetch = ''
+
         if self.path == '/':
             file_to_fetch = os.path.join('server_pages', 'search.html')
         elif self.path == '/imagesearch':
             file_to_fetch = os.path.join('server_pages', 'imagesearch.html')
+        elif '/pics/' in os.path.abspath(self.path):
+            file_to_fetch = self.path[1:]
         else:
             self.send_error(404,'File Not Found: {0}'.format(self.path))
             return
@@ -52,20 +82,19 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(f.read())
             f.close()
-            return
         except IOError:
             self.send_error(404,'File Not Found: {0}'.format(self.path))
 
     def do_POST(self):
+        form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type']})
+        self.send_response(200)
+        self.end_headers()
         if self.path=='/query':
-            form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type']})
-            print('Your query is: {0}'.format(form['query'].value))
-            self.send_response(200)
-            self.end_headers()
             self.wfile.write(str.encode(self.query_process(form['query'].value)))
-            return          
-            
-            
+        elif self.path=='/imagequery':
+            self.wfile.write(str.encode(self.imagequery_process(form['file'])))
+
+
 try:
     #Create a web server and define the handler to manage the
     #incoming request
